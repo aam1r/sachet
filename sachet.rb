7a2bd4bd430f33f9f36bee67a276503ffeda3e22
Aamir Mansoor <me@aam1r.com>
2013-02-15 06:38:40 -0500

    Add rules in configuration.yaml and add code to generate .vimrc
    Rules in configuration.yaml are now displayed as checkboxes. For the
    selected checkboxes, a .vimrc file is generated on-the-fly.

--- sachet.rb
+++ sachet.rb
@@ -1,9 +1,24 @@
 require 'sinatra'
 require 'yaml'
 
-get '/' do
-  configuration = YAML.load_file('data/configuration.yaml')
-  puts configuration.inspect
+CONFIGURATION = YAML.load_file('data/configuration.yaml')
 
-  erb :index, :locals => {:configuration => configuration}
+get '/' do
+  erb :index, :locals => {:configuration => CONFIGURATION}
+end
+
+post '/download' do
+  result = []
+  configuration_mapping = CONFIGURATION.group_by{|config| config['id']}
+
+  params.each do |param|
+    key = param.first
+
+    # append the vim config syntax
+    if configuration_mapping.has_key?(key)
+      result << configuration_mapping[key][0]['syntax']
+    end
+  end
+
+  result.join('<br>')
 end
