5ac39696a90e5cd46f150eaf5f28686a0b0889b2
Aamir Mansoor <me@aam1r.com>
2013-02-15 07:35:08 -0500

    Add themes support and generalize ERB template
    Generalize the ERB template so it uses a single loop to display all
    possible configurations (vim parameters, themes, plugin, etc.) instead
    of defining a new loop for each.
    
    Additionally, generalize the '/download' endpoint by constructing a
    single hash table consisting of data from all the data files.
    
    This allows us to easily extend the functionality of the application to
    support themes, plugins, etc.

--- sachet.rb
+++ sachet.rb
@@ -2,21 +2,35 @@ require 'sinatra'
 require 'yaml'
 
 CONFIGURATION = YAML.load_file('data/configuration.yaml')
+THEMES = YAML.load_file('data/themes.yaml')
 
 get '/' do
-  erb :index, :locals => {:configuration => CONFIGURATION}
+  erb :index, :locals => {:configuration => CONFIGURATION, :themes => THEMES}
 end
 
 post '/download' do
   result = []
-  configuration_mapping = CONFIGURATION.group_by{|config| config['id']}
 
+  # merge all data files and construct a key-value mapping
+  options = CONFIGURATION + THEMES
+  options_mapping = options.group_by{|config| config['id']}
+
+  # for each selected parameter, add the appropriate line into the generated
+  # vim.rc file
   params.each do |param|
     key = param.first
 
     # append the vim config syntax
-    if configuration_mapping.has_key?(key)
-      result << configuration_mapping[key][0]['syntax']
+    if options_mapping.has_key?(key)
+      config = options_mapping[key].first
+      syntax = config['syntax']
+
+      # replace tag with actual value
+      if config['input_type'] and config['tag']
+        syntax[config['tag']] = params[key]
+      end
+
+      result << syntax
     end
   end
 
