4a5a9974bc1843a9715688e17c27dd06b7ae8639
Aamir Mansoor <me@aam1r.com>
2013-02-15 13:09:36 -0500

    Pull-out method in refresh_repos
    Refactor refresh_repos and pull out code to make the 'fetch_repo'
    method. This allows us to keep the pathogen repo (which is a required
    plugin) up-to-date.

--- refresh_repos.rb
+++ refresh_repos.rb
@@ -12,11 +12,30 @@ PLUGINS = YAML.load_file('data/plugins.yaml')
 
 # Given a git url, return the name of the repository
 #   Example: "git://github.com/vim-scripts/Wombat.git" -> "Wombat"
-def repository_name(git_url)
-  last_chunk = git_url.split('/').last
+def repository_name(url)
+  last_chunk = url.split('/').last
   last_chunk['.git'] = ''
 
   last_chunk
+end
+
+def fetch_repo(url, target_dir='repos')
+  if url
+    repo_name = repository_name(url)
+    git_cmd = ''
+
+    if File.directory?("#{target_dir}/#{repo_name}")
+      # if folder exists already, do 'git pull'
+      puts "Doing git pull for " + repo_name
+      git_cmd = %x[cd #{target_dir}/#{repo_name}; git pull]
+    else
+      # clone the repo since it doesn't exist yet
+      puts "Cloning " + repo_name
+      git_cmd = %x[cd #{target_dir}; git clone #{url}]
+    end
+
+    puts git_cmd.inspect
+  end
 end
 
 # Parse through each configuration file with git urls and fetch HEAD for each
@@ -25,25 +44,12 @@ def refresh()
   options = THEMES + PLUGINS
 
   options.each do |option|
-    if option['url']
-      repo_name = repository_name(option['url'])
-      git_cmd = ''
-
-      if File.directory?('repos/' + repo_name)
-        # if folder exists already, do 'git pull'
-        puts "Doing git pull for " + repo_name
-        git_cmd = %x[cd repos/#{repo_name}; git pull]
-      else
-        # clone the repo since it doesn't exist yet
-        puts "Cloning " + repo_name
-        git_cmd = %x[cd repos; git clone #{option['url']}]
-      end
-
-      puts git_cmd.inspect
-    end
+    fetch_repo(option['url'])
   end
+
+  # Manually fetch pathogen
+  fetch_repo('git://github.com/tpope/vim-pathogen.git')
 end
 
-
-# Execute script
+# Execute script to fetch each plugin / theme
 refresh()
