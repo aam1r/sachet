8ef55f9c8782c47d8c446c9ce489d00c48579433
ashemsay <ashemsay@users.noreply.github.com>
2014-08-01 22:09:22 +0200

    Fix copy of vim directory
    The variable $sachet__install_home already contains a trailing slash, no need to add one when concatening the directory/file to it

--- install.sh
+++ install.sh
@@ -6,33 +6,34 @@
 # @usage:
 # Run this script from where the sachet was extracted to
 
+
 # sachet installation variables
 sachet__install_home=~/
 sachet__install_cwd="$(pwd)"
 
 # check if .vimrc already exists
-if [ -f "$sachet__install_home"/.vimrc ]; then
+if [ -f "$sachet__install_home".vimrc ]; then
   echo "Moving your ~/.vimrc to ~/.vimrc.old"
-  mv "$sachet__install_home"/.vimrc "$sachet__install_home"/.vimrc.old
+  mv "$sachet__install_home".vimrc "$sachet__install_home".vimrc.old
 
   echo ".. Successfully moved ~/.vimrc to ~/.vimrc.old"
 fi
 
 echo "Copying sachet's .vimrc to ~/.vimrc"
-cp "$sachet__install_cwd"/vimrc "$sachet__install_home"/.vimrc
+cp "$sachet__install_cwd"/vimrc "$sachet__install_home".vimrc
 
 echo ".. Successfully copied vimrc to ~/.vimrc"
 
 # check if .vim folder already exists
-if [ -d "$sachet__install_home"/.vim ]; then
+if [ -d "$sachet__install_home".vim ]; then
   echo "Moving your ~/.vim/ to ~/.vim.old/"
-  mv "$sachet__install_home"/.vim/ "$sachet__install_home"/.vim.old/
+  mv "$sachet__install_home".vim/ "$sachet__install_home".vim.old/
   
   echo ".. Successfully moved ~/.vim/ to ~/.vim.old/"
 fi
 
 echo "Copying sachet's .vim/ to ~/.vim/"
-cp -r "$sachet__install_cwd"/vim/* "$sachet__install_home"/.vim/
+cp -r "$sachet__install_cwd"/vim/ "$sachet__install_home".vim/
 
 echo ".. Successfully copied vim/ to ~/.vim/"
 
